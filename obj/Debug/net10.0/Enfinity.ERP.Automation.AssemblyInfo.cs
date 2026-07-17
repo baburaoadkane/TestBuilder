10f7c302e91d19f6c12eaf37500b39e93995f722
Baburao Adkane <b.g.adkane27@gmail.com>
2026-07-17 20:18:53 +0530

    Minor Change

--- obj/Debug/net10.0/Enfinity.ERP.Automation.AssemblyInfo.cs
+++ obj/Debug/net10.0/Enfinity.ERP.Automation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enfinity.ERP.Automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d28d27ea070820d681a046c79c6fb1bdb5b6f74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a435d28b9d73b9f8dd809dcbb493b166e5798ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enfinity.ERP.Automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enfinity.ERP.Automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
